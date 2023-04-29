443bfca96207554096f520a40c093f1a48184cea
gpappa01 <gpappa01@ucy.ac.cy>
2023-04-29 06:52:48 +0300

    Implement connection to DB
    Additional functionls like countOnlineCharacters might change. This is a
    delayed commit

--- l2jstats/src/main/java/com/l2eminence/App.java
+++ l2jstats/src/main/java/com/l2eminence/App.java
@@ -1,6 +1,5 @@
 package com.l2eminence;
 import java.util.Properties;
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 
@@ -12,8 +11,14 @@ public class App {
             e.printStackTrace();
             System.exit(1);
         }
+        System.out.println("Online characters: " + countOnlineCharacters(con));
+
     }
 
+    /**
+     * @return A connection with the db based on local.properties
+     * @throws SQLException
+     */
     private static Connection getConnection() throws SQLException{ 
         Connection connection = null;
         InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("resources/properties/database.properties");
@@ -39,5 +44,47 @@ public class App {
             throw new SQLException("Failed to establish mysql connection for unknown reason. Are the credentials correct?");
         }
         return connection;
+    }
+
+    private static int countOnlineCharacters(Connection con){
+        int count = 0;
+        try{
+            ResultSet replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS WHERE online=1");
+            while(replies.next()){
+                count = replies.getInt(1);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return count;
+    }
+
+    private static int countOfflineCharacters(Connection con){
+        int count = 0;
+        try{
+            ResultSet replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS WHERE online=0");
+            while(replies.next()){
+                count = replies.getInt(1);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return count;
+    }
+
+    private static int countAllCharacters(Connection con){
+        int count = 0;
+        try{
+            ResultSet replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS");
+            while(replies.next()){
+                count = replies.getInt(1);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return count;
     }
 }
