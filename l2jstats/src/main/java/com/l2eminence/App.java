09ef22ea25a2a9d4f803d5cede726cc9ef1d535f
gpappa01 <gpappa01@ucy.ac.cy>
2023-04-29 07:39:19 +0300

    Include json dependancy
    Updated pom.xml to request json and included it in main
    This is a convenient way to organise the queries to send to web
    server

--- l2jstats/src/main/java/com/l2eminence/App.java
+++ l2jstats/src/main/java/com/l2eminence/App.java
@@ -1,90 +1,68 @@
 package com.l2eminence;
+
 import java.util.Properties;
 import java.io.InputStream;
 import java.sql.*;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 public class App {
     public static void main(String[] args) {
-        Connection con = null;
-        try{con = getConnection();
-        }catch(Exception e){
-            e.printStackTrace();
-            System.exit(1);
-        }
-        System.out.println("Online characters: " + countOnlineCharacters(con));
-
+        System.out.println(getOnlineStatus().toString());
     }
-
     /**
      * @return A connection with the db based on local.properties
      * @throws SQLException
      */
-    private static Connection getConnection() throws SQLException{ 
+    private static Connection getConnection() throws SQLException {
         Connection connection = null;
-        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("resources/properties/database.properties");
+        InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("resources/properties/database.properties");
         Properties db_properties = new Properties();
-        try{
-        db_properties.load(input);
-        }catch(Exception e){
+        try {
+            db_properties.load(input);
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append("jdbc:mysql://");
-        sb.append(db_properties.getProperty("address")).append(":");
-        sb.append(db_properties.getProperty("port")).append("/");
-        sb.append(db_properties.getProperty("database"));
-        try{
-        connection = DriverManager.getConnection(sb.toString(), db_properties.getProperty("user"), db_properties.getProperty("password"));
-        }catch(Exception e){
+        try {
+            StringBuilder sb = new StringBuilder();
+            sb.append("jdbc:mysql://");
+            sb.append(db_properties.getProperty("address")).append(":");
+            sb.append(db_properties.getProperty("port")).append("/");
+            sb.append(db_properties.getProperty("database"));
+            connection = DriverManager.getConnection(sb.toString(), db_properties.getProperty("user"),
+                    db_properties.getProperty("password"));
+            input.close();
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
-        if(connection == null){
-            throw new SQLException("Failed to establish mysql connection for unknown reason. Are the credentials correct?");
+        if (connection == null) {
+            throw new SQLException(
+                    "Failed to establish mysql connection for unknown reason. Are the credentials correct?");
         }
         return connection;
     }
 
-    private static int countOnlineCharacters(Connection con){
-        int count = 0;
-        try{
+    private static JSONObject getOnlineStatus(){
+        int online = 0;
+        int offline = 0;
+        JSONObject json = new JSONObject();
+        try {
+            Connection con = getConnection();
             ResultSet replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS WHERE online=1");
-            while(replies.next()){
-                count = replies.getInt(1);
-            }
-        }catch(Exception e){
+            replies.next(); //only 1 return value from COUNT()
+            json.put("online", replies.getInt(1));
+            replies.close();
+            replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS WHERE online=0");
+            replies.next();
+            json.put("offline", replies.getInt(1));
+            con.close();
+        } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
-        return count;
-    }
-
-    private static int countOfflineCharacters(Connection con){
-        int count = 0;
-        try{
-            ResultSet replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS WHERE online=0");
-            while(replies.next()){
-                count = replies.getInt(1);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-            System.exit(1);
-        }
-        return count;
-    }
-
-    private static int countAllCharacters(Connection con){
-        int count = 0;
-        try{
-            ResultSet replies = con.createStatement().executeQuery("SELECT COUNT(*) FROM CHARACTERS");
-            while(replies.next()){
-                count = replies.getInt(1);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-            System.exit(1);
-        }
-        return count;
+        return json;
     }
 }
