f93912e08e7b584431b8d3e05a3ace3253cec94b
gpappa01 <gpappa01@ucy.ac.cy>
2023-05-01 12:20:34 +0300

    Include package info

---
+++ src/main/java/com/l2eminence/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This module offers GET services for a l2jgame database from l2jserver project
+ * @author https://github.com/Kam1goroshi
+ * @since 1 may 2023
+ * @version 1.0
+ * 
+ */
+package com.l2eminence;
